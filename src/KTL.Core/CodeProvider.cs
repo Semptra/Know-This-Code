f287be21fa62bbadd40a7ce1a0334a338ab47765
Oleksii Sachek <v-olsach@microsoft.com>
2019-07-29 09:30:16 +0300

    Initial project setup

---
+++ src/KTL.Core/CodeProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KTL.Core
+{
+    public sealed class CodeProvider
+    {
+        public string GetRandomCodeSample ()
+        {
+            string result = null;
+            
+            return result;
+        }
+    }
+}
